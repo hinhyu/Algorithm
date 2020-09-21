7548a24130f9a261068f4a3033bea52c1766f6b6
HongInhye <hinhyu@naver.com>
2020-09-21 18:19:48 +0900

    0921_2

--- 단계별문제풀이/10.수학2/05골드바흐의추측.py
+++ 단계별문제풀이/10.수학2/05골드바흐의추측.py
@@ -2,13 +2,27 @@ n=int(input())
 prime = []
 if n%2==0:
     for i in range(1, n+1):
-        if i != 1:#1이 아닌 요소(i)를 하나씩 꺼내면서 2~ i-1까지 나눠지는 수가 없을 때만을 골라낸다.
-            check = True
+        if i != 1:
             for j in range(2, i):
                 if i % j == 0:
                     check = False
                     break
             if check:
                 prime.append(i)
-    if prime(i) + prime(i) == n:
-        print(prime(i),prime(i))
+    if prime(x) + prime(y) == n:
+        print(prime(x),prime(y))
+
+#방법02
+arr = [0]*10001
+arr[1] = 1
+for i in range(2, 98):
+    for j in range(i * 2, 10001, i):
+        arr[j] = 1
+        
+t = int(input())
+for i in range(t):
+    n = int(input())
+    for j in range(n // 2, 1, -1):
+        if arr[n - j] == 0 and arr[j] == 0:
+            print(j, n - j)
+            break
--- 단계별문제풀이/11.재귀/03별찍기.py
+++ 단계별문제풀이/11.재귀/03별찍기.py
@@ -15,3 +15,37 @@ for i in range(n):
     for j in range(n):
         print(li[i][j], end='')
     print()
+
+#02방법
+
+def concat(r1, r2):
+    return [''.join(x) for x in zip(r1, r2, r1)]
+ 
+def star10(n):
+    if n == 1:
+        return ['*']
+    n //= 3
+    x = star10(n)
+    a = concat(x, x)
+    b = concat(x, [' '*n]*n)
+ 
+    return a + b + a
+ 
+print('\n'.join(star10(int(input()))))
+
+#join 연산 : "".join(list) : 리스트에서 문자열로 합쳐줌
+# time_list
+# ['10','34','17']
+# ':'.join(time_list)
+# '10:34:17'
+
+#zip 함수 : 같은 길이의 리스트를 같은 인덱스끼리 잘라서 리스트로 반환해주는 역할
+# name=['a','b']
+# value=[1,2]
+
+# # for n,v in zip(name, value):
+# #     print(n,v)
+
+# # 결과
+# # a 1
+# # b 2
