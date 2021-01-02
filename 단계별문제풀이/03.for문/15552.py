ed67b6b8c1a06c6d508eae1037b420c3ffeb7061
HongInhye <hinhyu@naver.com>
2021-01-02 13:00:56 +0900

    20200102-2

--- 단계별문제풀이/03.for문/15552.py
+++ 단계별문제풀이/03.for문/15552.py
@@ -4,3 +4,5 @@ t = int(input())
 for i in range(t):
         a,b = map(int, sys.stdin.readline().split())
         print(a+b)
+
+#Python에서 입력값을 받을 때 input() 함수를 사용하지만 시간단축을 위해 sys.stdin.readline을 사용한다.
